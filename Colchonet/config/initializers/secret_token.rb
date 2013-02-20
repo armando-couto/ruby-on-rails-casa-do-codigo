948837bfc57930e148b07c08f7a0065eacc53dcf
Armando Couto <armando-couto@hotmail.com>
2013-02-20 00:42:36 -0300

    Projeto completo.

---
+++ Colchonet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colchonet::Application.config.secret_token = 'c58d983136beec2c3909b13ca394439066dfcdccef7d25dfbcf9271295e9b2a970d6981fa7deb2a593391cc3c63ed842bb03781755ef558b75c617fa44959a28'
